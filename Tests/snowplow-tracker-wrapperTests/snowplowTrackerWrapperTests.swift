e7e75190d4fc17ab46595c6e53d540f3b4c6e956
Luiz Hammerli <luizhammerli@gmail.com>
2021-10-27 10:49:50 -0300

    - Fix test class name removing "_" char

--- Tests/snowplow-tracker-wrapperTests/snowplowTrackerWrapperTests.swift
+++ Tests/snowplow-tracker-wrapperTests/snowplowTrackerWrapperTests.swift
@@ -1,7 +1,7 @@
     import XCTest
     @testable import snowplow_tracker_wrapper
 
-    final class snowplow_tracker_wrapperTests: XCTestCase {
+    final class snowplowTrackerWrapperTests: XCTestCase {
         func testExample() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
